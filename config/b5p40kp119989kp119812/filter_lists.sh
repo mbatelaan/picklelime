b28d2661fa36434489a7b27bb19dba02c66d0694
hhpmbate <hhpmbate@blogin5.usr.hlrn.de>
2023-02-10 06:53:40 +0100

    config and script changes

---
+++ config/b5p40kp119989kp119812/filter_lists.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_000.lst 
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_200.lst 
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_222.lst 
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_420.lst 
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_440.lst 
+
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_src2_000.lst 
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_src2_200.lst 
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_src2_222.lst 
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_src2_420.lst 
+sed -i "s=smsnk30=ptsink=g" b5p40kp119989kp119812_src2_440.lst 
